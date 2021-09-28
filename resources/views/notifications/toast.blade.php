a3454bf1b9a88812d4dfe9f746e68db3ea523567
sanlf <35877936+sanlf@users.noreply.github.com>
2021-09-28 12:00:41 -0700

    Fix typo: error toast

--- resources/views/notifications/toast.blade.php
+++ resources/views/notifications/toast.blade.php
@@ -42,6 +42,7 @@
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"/>
                                 </svg>
                             </div>
+
                         @endif
                         <div class="ml-4 w-0 flex-1">
 
